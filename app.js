ca94e4e7aea826bc178cbaacd702a42272d07c13
leasmo23 <leasmo23@bergen.org>
2022-06-06 16:00:28 -0400

    6/6 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require( "express" );
 const logger = require("morgan" );
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 
 // Configure Express to use EJS
 app.set( "views",  __dirname + "/views");
